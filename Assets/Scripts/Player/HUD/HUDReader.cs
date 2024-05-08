151e0dc692de4a828b317549dbefee7f76b78d86
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2024-05-08 12:01:19 -0500

    working on player hud

--- Assets/Scripts/Player/HUD/HUDReader.cs
+++ Assets/Scripts/Player/HUD/HUDReader.cs
@@ -8,17 +8,51 @@ public class HUDReader : MonoBehaviour
     public PlayerStatus player;
     private List<PlayerStatus> currentPlayers;
     //this will subscribe to all events
-    //damage/ health 
+    //damage/ health
+    [SerializeField] Transform PlayerHolder;
+    [SerializeField] Transform PlayerUIPrefab;
+
+    //[SerializeField] Timer timer;
+
+    //seperate player UI logic for mulitple players?-> player status
+
 
     void Start()
     {
         
     }
 
+    private void Update()
+    {
+        //on space create new player to get UI better
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+
+
+        }
+
+    }
+
     public void Init()
     {
      //return the player   
     }
+
+    void HealthUI(int HealthPoints)
+    {
+
+    }
+
+    void ItemUI()
+    {
+
+    }
+
+    void LivesUI()
+    {
+
+    }
+
 
     /*
      * public static IList<T> Swap<T>(this IList<T> list, int indexA, int indexB)
