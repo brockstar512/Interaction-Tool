3a9bd14162135bef0edd43d115b890e8d83e95b5
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2024-07-07 09:02:26 -0600

    trying to just pass interface

--- Assets/Scripts/Interface/IStateMachine.cs
+++ Assets/Scripts/Interface/IStateMachine.cs
@@ -1,21 +1,14 @@
+using UnityEngine;
+
 namespace Interface
 {
     public interface IStateMachine
     {
-        //    public IOverlapCheck OverlapObjectCheck;
-        //    public Vector2 Movement { get; private set; }
-        //    public Rigidbody2D rb { get; private set; }
-        //    public BoxCollider2D col { get; private set; }
-        //    public PlayerBaseState GetState { get { return currentState; } }
-        //    public InteractableBase item { get; private set; }
-        //    public Animator animator { get; private set; }
-
-        
-
-
-
-
-
+        public PlayerBaseState currentState{ get; }
+        public Vector2 movement { get;}
+        public Animator animator { get; }
+        public ItemManager itemManager { get;}
+        public Rigidbody2D rb { get; }
 
     }
 }
