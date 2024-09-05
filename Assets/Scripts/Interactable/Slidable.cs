ce874020c3b17c3d6567534a52fe9460093e47ef
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2024-09-05 12:19:24 -0500

    get min distance

--- Assets/Scripts/Interactable/Slidable.cs
+++ Assets/Scripts/Interactable/Slidable.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using DG.Tweening.Core;
 using Player.ItemOverlap;
 using Unity.VisualScripting;
+using System.Linq;
 
 
 [RequireComponent(typeof(SpriteRenderer))]
@@ -32,7 +33,7 @@ public class Slidable : InteractableBase
 
     private void FixedUpdate()
     {
-        GetClosestRaycastHit(Vector2.left);
+        GetClosestRaycastHit(Vector2.right);
     }
 
     public override bool Interact(PlayerStateMachineManager state)
@@ -67,26 +68,64 @@ public class Slidable : InteractableBase
 
         RaycastHit2D hitSideOne = Physics2D.Raycast(sideOnePos, direction ,int.MaxValue,obstructionLayer);
         Debug.DrawRay(sideOnePos,direction, Color.blue);
-
-
+        
         RaycastHit2D hitMiddle = Physics2D.Raycast(startPos, direction ,int.MaxValue,obstructionLayer);
         Debug.DrawRay(startPos,direction, Color.red);
 
         RaycastHit2D hitSideTwo = Physics2D.Raycast(sideTwoPos, direction ,int.MaxValue,obstructionLayer);
         Debug.DrawRay(sideTwoPos,direction, Color.green);
-
         
-        // if (direction == Vector2.left || direction == Vector2.right)
-        // {
-        //     float distanceOne = hitSideOne.collider !=null ? Mathf.Abs(hitSideOne.transform.position.x -this.transform.position.x) : int.MaxValue;
-        //     float distanceTwo = hitSideOne.collider !=null ? Mathf.Abs(hitSideTwo.transform.position.x -this.transform.position.x): int.MaxValue;
-        //     float distanceMiddle =  hitSideOne.collider !=null ?Mathf.Abs(hitMiddle.transform.position.x -this.transform.position.x) : int.MaxValue;
-        //
-        //
-        // }   
+            // float distanceOne = float.MaxValue;
+            // float distanceTwo = float.MaxValue;
+            // float distanceMiddle = float.MaxValue;
+      
+        if (direction == Vector2.left || direction == Vector2.right)
+        {
+            float[] numbers = new float[]
+            {
+                float.MaxValue,
+                float.MaxValue,
+                float.MaxValue,
+            };
+
+            if (hitSideOne.collider!=null)
+            {
+                numbers[0] = Mathf.Abs(hitSideOne.transform.position.x - this.transform.position.x);
+
+            }
+            if (hitSideTwo.collider!=null)
+            {
+                numbers[1] = Mathf.Abs(hitSideTwo.transform.position.x -this.transform.position.x);
+
+            }
+            if (hitMiddle.collider!=null)
+            {
+                numbers[2] = Mathf.Abs(hitMiddle.transform.position.x - this.transform.position.x);
+
+            }
+            
+            //float distanceOne = hitSideOne.collider!=null ? Mathf.Abs(hitSideOne.transform.position.x -this.transform.position.x) : int.MaxValue;
+            Debug.Log(numbers[0]);
+           // float distanceTwo = hitSideTwo.collider==null ? int.MaxValue: Mathf.Abs(hitSideTwo.transform.position.x -this.transform.position.x);
+            Debug.Log(numbers[1]);
+            //float distanceMiddle =  hitMiddle.collider!=null ?Mathf.Abs(hitMiddle.transform.position.x -this.transform.position.x) : int.MaxValue;
+            Debug.Log(numbers[2]);
+
+            float singleCoordDestination = numbers.Min();
+            Debug.Log("Winner: "+ singleCoordDestination);
+
+
+
+
+        }   
 
 
         return resultingHit;
+    }
+
+    float GetAbsoluteValueOfDistance(float from, float to)
+    {
+        return Mathf.Abs(from - to);
     }
     
 
