4ce6c2d96515eba57351714d369dd66f49a86b4e
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2024-11-10 14:15:19 -0600

    importing urp

--- Assets/Scripts/AI/EnemyPlaceholder.cs
+++ Assets/Scripts/AI/EnemyPlaceholder.cs
@@ -6,7 +6,7 @@ using Items.SubItems;
 
 public class EnemyPlaceholder : MonoBehaviour, IHurt, IInteractWithHookProjectile
 {
-    
+    //todo i need to handle the logic with how the grappling hook hits the ememy
     public void ApplyDamage(IDamage damagingThing)
     {
         Debug.Log("apply damage");
--- Assets/Scripts/Interactable/Throwable.cs
+++ Assets/Scripts/Interactable/Throwable.cs
@@ -9,40 +9,52 @@ using UnityEngine.UI;
 public class Throwable : InteractableBase, IInteractWithHookProjectile
 {
 
-
-    Vector3 startingPoint;
-    float distanceFromGround;
-    float DISTANCE_LIMIT = 7.5f;//anymore will go in a straight line when it reaches point
-    bool isThrown = false;
-    Vector2 throwDirection;
-    const float SPEED = 12f;
+    //how fast it will be thrown
+    const float Speed = 12f;
+    //how far it will go...
+    //anymore will go in a straight line when it reaches point
+    const float DistanceLimit = 7.5f;
+    //cached starting point so the distance
+    Vector3 _startingPoint;
+    //how high it will go
+    float _distanceFromGround;
+    //so we can keep track of how far it is going
+    bool _isThrown = false;
+    //which direction to make it thrown
+    Vector2 _throwDirection;
+    //what we are throwing
     [SerializeField] Transform throwable;
+    //the shadow to give it the illusion of height
     [SerializeField] Transform shadow;
+    //the tween arch for it's path
     [SerializeField] AnimationCurve curve;
 
     protected void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        //update the layer so we can interact with it
         UpdateLayerName();
     }
 
     public override bool Interact(PlayerStateMachineManager player)
     {
+        //pick up the item and set the parent as the player
         PickUp(player.transform);
-        //if it's too heavy or there is a status on it I can add more complex logic of it I can or can't pick it up
         return true;
     }
 
     protected void FixedUpdate()
     {
-        if (isThrown)
+        //if it's thrown run the in air logic
+        if (_isThrown)
         {
             InAir();
         }
-
-        if (isThrown && Vector2.Distance(startingPoint, transform.position) >= DISTANCE_LIMIT)
+        //if it's thrown and we have reached the distance limit
+        //destroy it
+        if (_isThrown && Vector2.Distance(_startingPoint, transform.position) >= DistanceLimit)
         {
-            isThrown = false;
+            _isThrown = false;
             Destroy(this.gameObject);
 
         }
@@ -50,60 +62,66 @@ public class Throwable : InteractableBase, IInteractWithHookProjectile
 
     protected Throwable PickUp(Transform parent)
     {
+        //when we pick it up set the trigger to true so it doesn't interact with anything else
+        //todo consider updating the layer as well if there is a problem with anyone else picking it
+        //up while it's picked up
         this.transform.GetComponent<BoxCollider2D>().isTrigger = true;
+        //set the parent as the player
         this.transform.SetParent(parent);
+        //move it so it looks good...this will change if I just get a sprite of him carrying it
         this.transform.localPosition = new Vector3(0, 0, 0);//+ 2.32f
         throwable.localPosition = new Vector3(0, 2.32f, 0); 
-
+        //this is not being cached/used elsewahere but we are still returning it
         return this;
     }
 
     protected void Toss(Vector3 direction)
     {
+        //turn on the shadow when it is thrown
         shadow.gameObject.SetActive(true);
+        //move the shadow
         shadow.position = new Vector3(shadow.position.x, shadow.position.y , shadow.position.z);//- .25f
+        //remove it as the child of player
         this.transform.SetParent(null);
-        startingPoint = transform.position;
-        throwDirection = direction;
-        isThrown = true;
+        //cache where it starts so we can keep track of the distance
+        _startingPoint = transform.position;
+        //cache the direction
+        _throwDirection = direction;
+        //set the boolean to true that it is thrown
+        _isThrown = true;
 
     }
-    //the tranform of the parent is straight. the throwable follows a curve
+
     protected void InAir()
     {
         Vector3 travelPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         //x is height
-        float yPos = curve.Evaluate(Vector2.Distance(startingPoint, travelPos));
-        //change the size of the shadow based on height
-        //scaledValue = (rawValue - min) / (max - min);
-        //float scale = Mathf.Clamp(yPos,.1f,1f);
-        //float scale = yPos / 3.5f;
-        //float scale = Mathf.Clamp(yPos,.25f,.75f);
-
-        //shadow.transform.localScale = new Vector3(1-scale, 1-scale, 1);
-            //(yPos - .2f) / (1 - .2f);
-        //Debug.Log(scale);
-
-        //Debug.Log(yPos);
+        float yPos = curve.Evaluate(Vector2.Distance(_startingPoint, travelPos));
+  
         throwable.localPosition = new Vector3(0, yPos, 0);
-        rb.MovePosition(rb.position + throwDirection * SPEED * Time.deltaTime);
+        rb.MovePosition(rb.position + _throwDirection * Speed * Time.deltaTime);
     }
 
     public override void Release(PlayerStateMachineManager state)
     {
-        distanceFromGround = state.transform.GetComponent<SpriteRenderer>().bounds.size.y;
+        //when we leave the state release will be called and it will be thrown and we immediately go to 
+        //default state
+        _distanceFromGround = state.transform.GetComponent<SpriteRenderer>().bounds.size.y;
+        //set the animation tween values
         Keyframe[] keyframes = curve.keys;
-        keyframes[0].value = distanceFromGround;
+        keyframes[0].value = _distanceFromGround;
         keyframes[1].value = 0;
-
         curve.keys = keyframes;
-
+        //throw the item 
         Toss(state.currentState.LookDirection);
     }
     
     public void InteractWithHookProjectile(HookProjectile projectile)
     {
+        //todo if this is a bomb set the item to null for when it explodes...or in the switch statement check if the bomb has exploded
+        //se the parent to the grappling hook
         this.transform.parent = projectile.transform;
+        //set it as a trigger so it can be retracted without any interuption on the way back
         this.GetComponent<Collider2D>().isTrigger = true;
     }
 }
