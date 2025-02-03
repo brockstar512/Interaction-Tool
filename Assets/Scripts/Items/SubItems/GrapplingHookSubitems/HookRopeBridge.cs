3937903e084840f1a92a345a76d26c71dc9d6664
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2025-02-02 22:37:09 -0600

    rope walls work. try and see if get all colliders needs to be async

--- Assets/Scripts/Items/SubItems/GrapplingHookSubitems/HookRopeBridge.cs
+++ Assets/Scripts/Items/SubItems/GrapplingHookSubitems/HookRopeBridge.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Threading.Tasks;
-using Player.ItemOverlap;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
@@ -9,8 +7,6 @@ public class HookRopeBridge : MonoBehaviour
     private LineRenderer _lineRenderer;
     [SerializeField] private Vector2 bridgeDirection;
     private BoxCollider2D _collider;
-    private BoxCollider2D _sideOne;
-    private BoxCollider2D _sideTwo;
     private Collider2D[] _depthsCol;
     private IGetAllOverlap<Collider2D> overlappingDepth { get; set; }
 
@@ -35,23 +31,29 @@ public class HookRopeBridge : MonoBehaviour
         _depthsCol = await overlappingDepth.GetAllOverlapObject(_lineRenderer.bounds);
         BuildWalls(_depthsCol,Utilities.GetDirectionFromTwoPoints(start, end));
     }
+
     void BuildWalls(Collider2D[] _depthsCol,Vector2 direction)
     {
-        return;
-        Debug.Log(direction);
+        Transform ropeTrans = transform;
+        float resizeVal = .25f;
 
+        
         void HorizontalWall()
         {
             foreach (var col in _depthsCol)
             {
+                Vector2 moveDestination = new Vector2(col.bounds.center.x - ropeTrans.position.x,_collider.offset.y);
+                SpriteRenderer colliderSprite = col.transform.GetComponent<SpriteRenderer>();
                 BoxCollider2D colUp = gameObject.AddComponent<BoxCollider2D>();
                 BoxCollider2D colDown  = gameObject.AddComponent<BoxCollider2D>();
-                // _sideOne.offset = _collider.offset;
-                // _sideTwo.offset = _collider.offset;
-                // _sideOne.size = _collider.size;
-                // _sideTwo.size = _collider.size;
-                // _sideTwo.offset += Vector2.right;
-                // _sideOne.offset += Vector2.left;
+                colUp.offset = moveDestination;
+                colDown.offset = moveDestination;
+                Vector2 resizeCompensate = new Vector2(0,resizeVal);
+                colUp.size = new Vector2(colliderSprite.bounds.size.x,resizeVal * 2);
+                colDown.size = new Vector2(colliderSprite.bounds.size.x,resizeVal * 2);
+                colUp.offset += Vector2.up - resizeCompensate;
+                colDown.offset += Vector2.down + resizeCompensate;
+
             }
         }
         
@@ -59,14 +61,25 @@ public class HookRopeBridge : MonoBehaviour
         {
             foreach (var col in _depthsCol)
             {
+                Vector2 moveDestination = new Vector2(_collider.offset.x, col.bounds.center.y - ropeTrans.position.y);
+                SpriteRenderer colliderSprite = col.transform.GetComponent<SpriteRenderer>();
                 BoxCollider2D colRight = gameObject.AddComponent<BoxCollider2D>();
                 BoxCollider2D colLeft  = gameObject.AddComponent<BoxCollider2D>();
-                // _sideOne.offset = _collider.offset;
-                // _sideTwo.offset = _collider.offset;
-                // _sideOne.size = _collider.size;
-                // _sideTwo.size = _collider.size;
-                // _sideTwo.offset += Vector2.right;
-                // _sideOne.offset += Vector2.left;
+                colRight.offset = moveDestination;
+                colLeft.offset = moveDestination;
+                //resize so it is smaller and we can compansate for it's size not being 1...
+                //_collider.size.x=size of the rope. we don't need it to be bulky so
+                //i'll make it .25f but it's .25f from its center so it needs to be moves back
+                //a little from the difference that it's i taken off of it meaning if I am moving
+                //right it's colRight.offset -= (.25f * 2) it's *2 because it needs to compensate 
+                //for it's center but this does not work for other compensation resizes ...
+                //i can redo the math and figure it out if i want that flexibilty to resize and adjust dynamically
+                //(if I want it same size as rope bridge it was _collider.size.x)
+                Vector2 resizeCompensate = new Vector2(resizeVal,0);
+                colRight.size = new Vector2(resizeVal * 2, colliderSprite.bounds.size.y);
+                colLeft.size = new Vector2(resizeVal * 2, colliderSprite.bounds.size.y);
+                colRight.offset += Vector2.right - resizeCompensate;
+                colLeft.offset += Vector2.left + resizeCompensate;
             }
         }
 
@@ -81,10 +94,8 @@ public class HookRopeBridge : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.layer == LayerMask.NameToLayer(Utilities.PlayerLayer))
         {
-            // Physics2D.IgnoreCollision(other.GetComponent<Collider2D>(), blackBoxCollider, true);
-
             foreach (var col in _depthsCol)
             {
                 Physics2D.IgnoreCollision(other.GetComponent<Collider2D>(), col, true);
@@ -94,10 +105,8 @@ public class HookRopeBridge : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-
-        if (other.CompareTag("Player"))
+        if (other.gameObject.layer == LayerMask.NameToLayer(Utilities.PlayerLayer))
         {
-            // Physics2D.IgnoreCollision(other.GetComponent<Collider2D>(), blackBoxCollider, false);
             foreach (var col in _depthsCol)
             {
                 Physics2D.IgnoreCollision(other.GetComponent<Collider2D>(), col, false);
