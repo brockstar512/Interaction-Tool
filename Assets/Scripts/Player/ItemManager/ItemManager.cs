c64f597b97a4851ebfae165160c8588ffac8df86
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2026-05-30 14:09:00 -0500

    updated item manager and removed remaining empty scripts

--- Assets/Scripts/Player/ItemManager/ItemManager.cs
+++ Assets/Scripts/Player/ItemManager/ItemManager.cs
@@ -19,10 +19,8 @@ public class ItemManager : MonoBehaviour, IItemManager
 
     public IItem GetItem()
     {
-        if(inventory.Count<= 0)
-        {
-            return null;
-        }
+        if (inventory.Count == 0) return null;
+        _currentIndex = Mathf.Clamp(_currentIndex, 0, inventory.Count - 1);
         return inventory[_currentIndex];
     }
    
@@ -76,20 +74,16 @@ public class ItemManager : MonoBehaviour, IItemManager
 
     public Sprite GetCurrentSprite()
     {
+        if (inventory.Count == 0) return null;
         return inventory[_currentIndex].Sprite;
     }
     public void DisposeOfCurrentItem()
     {
         inventory.RemoveAt(_currentIndex);
-        //if we are at the end and the item index is not 0 we are going to decrement the index
-        //otherwise the item that was ahead will fall down to the current index
-        //if it's not 0 that will help in the event that you can only have one item
-        _currentIndex = _currentIndex == inventoryLimit &&_currentIndex!=0  ? _currentIndex-- : _currentIndex;
-        //update UI to match the inventory
-        Sprite newSprite = (_currentIndex > inventory.Count)||(inventory.Count == 0) ? null : inventory[_currentIndex].Sprite;
+        if (_currentIndex >= inventory.Count && _currentIndex > 0)
+            _currentIndex--;
+        Sprite newSprite = inventory.Count == 0 ? null : inventory[_currentIndex].Sprite;
         ItemSwitch?.Invoke(newSprite);
-
-
     }
     
 }
